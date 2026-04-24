702a219a110acb55b6529e6a2d14c1abae85b16c
Yatvesh1107 <yatvesh1107@gmail.com>
2026-04-24 17:34:18 +0530

    added member panel api

--- routes/fitnessMemberPanelRoutes.js
+++ routes/fitnessMemberPanelRoutes.js
@@ -4,12 +4,22 @@ const router = express.Router();
 const auth = require("../middleware/auth");
 const { allowPermissions } = require("../middleware/permissions");
 
+
 const {
   getMemberDashboard,
   getMemberProfile,
-  getMemberMembershipSummary
+  getMemberMembershipSummary,
+  getAvailableActivities,
+  createMemberBooking,
+  getMemberBookings,
+  getMemberPayments,
+  getActivityFeePlans,
+  createMembershipOrder,
+  verifyMembershipPayment,
+  createRenewalOrder,
+  verifyRenewalPayment,
+  getMemberMemberships
 } = require("../controllers/fitnessMemberPanelController");
-
 
 // ===============================
 // MEMBER DASHBOARD
@@ -43,4 +53,132 @@ router.get(
   getMemberMembershipSummary
 );
 
+
+//===============================
+// Available activities
+// ===============================
+
+router.get(
+  "/available-activities",
+  auth,
+  allowPermissions("VIEW_ACTIVITIES"),
+  getAvailableActivities
+);
+
+
+//===============================
+// Booking activities
+// ===============================
+
+router.post(
+  "/book-slot",
+  auth,
+  allowPermissions("BOOK_ACTIVITY"),
+  createMemberBooking
+);
+
+//===============================
+// Booking History
+// ===============================
+router.get(
+  "/bookings",
+  auth,
+  allowPermissions("VIEW_BOOKINGS"),
+  getMemberBookings
+);
+
+
+//===============================
+// Payment  History
+// ===============================
+
+router.get(
+  "/payments",
+  auth,
+  allowPermissions("VIEW_PAYMENTS"),
+  getMemberPayments
+);
+
+//===============================
+// Activity fees
+// ===============================
+
+router.get(
+  "/activity-fees/:activityId",
+  auth,
+  allowPermissions("VIEW_ACTIVITIES"),
+  getActivityFeePlans
+);
+
+
+//===============================
+// Razorpay membership purchase order
+// ===============================
+
+router.post(
+  "/create-membership-order",
+  auth,
+  allowPermissions("BOOK_ACTIVITY"),
+  createMembershipOrder
+);
+
+//===============================
+// Razorpay  payment verification
+// ===============================
+
+router.post(
+  "/verify-membership-payment",
+  auth,
+  allowPermissions("BOOK_ACTIVITY"),
+  verifyMembershipPayment
+);
+
+//===============================
+// Renewal order
+// ===============================
+
+router.post(
+  "/create-renewal-order",
+  auth,
+  allowPermissions("VIEW_MEMBERSHIP"),
+  createRenewalOrder
+);
+
+
+//===============================
+// Verify Renewal order
+// ===============================
+
+router.post(
+  "/verify-renewal-payment",
+  auth,
+  allowPermissions("VIEW_MEMBERSHIP"),
+  verifyRenewalPayment
+);
+
+
+//===============================
+// Membership status
+// ===============================
+
+
+router.get(
+  "/memberships",
+  auth,
+  allowPermissions("VIEW_MEMBERSHIP"),
+  getMemberMemberships
+);
+
+
+
+//===============================
+// Member Profile routes
+// ===============================
+
+router.get(
+  "/profile",
+  auth,
+  allowPermissions("VIEW_OWN_PROFILE"),
+  getMemberProfile
+);
 module.exports = router;
