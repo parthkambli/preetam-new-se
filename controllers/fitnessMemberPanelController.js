38357632eb512dbdef4bac9e488b8c80c3ca2d27
Yatvesh1107 <yatvesh1107@gmail.com>
2026-05-05 11:41:58 +0530

    member panel by pass removed

--- controllers/fitnessMemberPanelController.js
+++ controllers/fitnessMemberPanelController.js
@@ -2483,19 +2483,19 @@ exports.verifyMembershipPayment = async (req, res) => {
     // ======================================
 
     // TEMPORARY DEV TEST ONLY
-    console.log("Razorpay verification bypassed for local testing");
+    // console.log("Razorpay verification bypassed for local testing");
 
-    // const generatedSignature = crypto
-    //   .createHmac("sha256", process.env.RAZORPAY_KEY_SECRET)
-    //   .update(`${razorpay_order_id}|${razorpay_payment_id}`)
-    //   .digest("hex");
+    const generatedSignature = crypto
+      .createHmac("sha256", process.env.RAZORPAY_KEY_SECRET)
+      .update(`${razorpay_order_id}|${razorpay_payment_id}`)
+      .digest("hex");
 
-    // if (generatedSignature !== razorpay_signature) {
-    //   return res.status(400).json({
-    //     success: false,
-    //     message: "Payment verification failed"
-    //   });
-    // }
+    if (generatedSignature !== razorpay_signature) {
+      return res.status(400).json({
+        success: false,
+        message: "Payment verification failed"
+      });
+    }
     
 
     // ======================================
